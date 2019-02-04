c320feb863c281add4367d88dfb5f4ab5bcff327
Artem Payvin <payvinartem@gmail.com>
2019-02-04 19:23:53 +0200

    SKALE-741 new deployment script

--- proxy/contracts/MessageProxy.sol
+++ proxy/contracts/MessageProxy.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.24;
+pragma solidity ^0.4.24;
 
 interface ContractReceiver {
     function postMessage(address sender, string schainID, address to, uint amount) external;
---
+++ proxy/migrations/1_deploy_contracts_to_mainnet.js
@@ -0,0 +1,72 @@
+const fs = require("fs");
+const path = require("path");
+const solc = require("solc");
+
+//let MessageProxy = artifacts.require("./MessageProxy.sol");
+//let DepositBox = artifacts.require("./DepositBox.sol");
+//let TokenManager = artifacts.require("./TokenManager.sol");
+let Ownable = path.resolve(__dirname, '../contracts', 'Ownable.sol');
+let MessageProxy = path.resolve(__dirname, '../contracts', 'MessageProxy.sol');
+let DepositBox = path.resolve(__dirname, '../contracts', 'DepositBox.sol');
+
+const networkName = process.env.NETWORK;
+const privateKey = process.env.ETH_PRIVATE_KEY;
+
+let networks = require("../truffle.js");
+let currentNetwork = networks['networks'][networkName];
+console.log(networkName);
+console.log(currentNetwork);
+
+const LINE = '======================================';
+
+const Web3 = require('web3');
+const PrivateKeyProvider = require("truffle-privatekey-provider");
+const provider = new PrivateKeyProvider(privateKey, `http://${currentNetwork['host']}:${currentNetwork['port']}`);
+const web3beta = new Web3(provider);
+
+const account = web3beta['_provider']['address'];
+
+async function deployContract(contractName, contract, options) {
+    let object = solc.compile(contract, 1);
+    console.log(LINE);
+    console.log(`Deploying: ${contractName}, options: ${JSON.stringify(options)}`);
+    const contractObj = new web3beta.eth.Contract(JSON.parse(object.contracts[contractName].interface));
+    const result = await contractObj.deploy({data: '0x' + object.contracts[contractName].bytecode, arguments: options['arguments']})
+        .send({gas: options['gas'], from: options['account'], value: options['value']});
+    //console.log(result);
+    console.log(`${contractName} deployed to: ${result.options.address}`);
+    return {receipt: result, contract: contractObj, address: result.options.address, abi: JSON.parse(object.contracts[contractName].interface)};
+}
+
+async function deploy() {
+    console.log('Attempting to deploy from account: ', account);
+
+    let messageProxy = {
+        'Ownable.sol': fs.readFileSync(Ownable, 'UTF-8'),
+        'MessageProxy.sol': fs.readFileSync(MessageProxy, 'UTF-8')
+    }
+    let messageProxyResult0 = await deployContract("MessageProxy.sol:MessageProxy", {sources: messageProxy}, {gas: 8000000, 'account': account, 'arguments': ["Mainnet"]});
+    let depositBox = {
+        'Ownable.sol': fs.readFileSync(Ownable, 'UTF-8'),
+        'DepositBox.sol': fs.readFileSync(DepositBox, 'UTF-8')
+    }
+    let depositBoxResult = await deployContract("DepositBox.sol:DepositBox", {sources: depositBox}, {gas: 8000000, 'account': account, 'arguments': [messageProxyResult0.address]});  
+
+
+    let jsonObject = {
+        deposit_box_address: depositBoxResult.address,
+        deposit_box_abi: DepositBox.abi,
+        message_proxy_mainnet_address: messageProxyResult0.address,
+        message_proxy_mainnet_abi: MessageProxy
+    }
+
+    fs.writeFile('proxyMainnet.json', JSON.stringify(jsonObject), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(`Done, check proxyMainnet.json file in data folder.`);
+        process.exit(0);
+    });
+}
+
+deploy();
