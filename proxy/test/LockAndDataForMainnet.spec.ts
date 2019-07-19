382c5a69021ea83d1b291f24bc2499fc852c2c4a
elvisgiv <alexeymaratovich@gmail.com>
2019-07-19 18:06:51 +0300

    SKALE-1297 Add tests for LockAndDataForMainnet.sol

--- proxy/test/LockAndDataForMainnet.spec.ts
+++ proxy/test/LockAndDataForMainnet.spec.ts
@@ -31,44 +31,92 @@ contract("LockAndDataForMainnet", ([deployer, user, invoker]) => {
        {from: deployer, gas: 8000000 * gasMultiplier});
   });
 
-  it("should add wei to lockAndDataForMainnet", async () => {
+  it("should add wei to `lockAndDataForMainnet`", async () => {
+    // preparation
     const wei = "10000";
     const lockAndDataBalanceBefore = await web3.eth.getBalance(lockAndDataForMainnet.address);
-    console.log("lockAndDataForMainnetBefore", lockAndDataBalanceBefore);
+    // add wei to contract throught receiveEth because receiveEth have `payable` parameter
     await lockAndDataForMainnet
       .receiveEth(invoker, {value: wei, from: deployer});
     const lockAndDataBalanceAfter = await web3.eth.getBalance(lockAndDataForMainnet.address);
-    console.log("lockAndDataForMainnetAfter", lockAndDataBalanceAfter);
+    // expectation
     expect(parseInt(lockAndDataBalanceAfter, 10) -
       parseInt(lockAndDataBalanceBefore, 10)).to.be.equal(parseInt(wei, 10));
   });
 
   it("should rejected with `Not enough ETH` when invoke sendEth", async () => {
+    // preparation
     const wei = "1000";
     const error = "Not enough ETH";
+    // add wei to contract throught receiveEth because receiveEth have `payable` parameter
     await lockAndDataForMainnet
       .receiveEth(invoker, {value: wei, from: deployer});
+    // execution/expectation
     await lockAndDataForMainnet
       .sendEth(invoker, 10000,
         {from: deployer})
       .should.be.eventually.rejectedWith(error);
-    const lockAndDataBalance = await web3.eth.getBalance(lockAndDataForMainnet.address);
-    console.log("lockAndDataForMainnet", lockAndDataBalance);
   });
 
-  it("should return `true` when invoke sendEth", async () => {
+  it("should work `sendEth`", async () => {
+    // preparation
     const addWeiToContract = "1000";
     const sendWeiFromContract = 100;
-    const lockAndDataBalanceBefore = await web3.eth.getBalance(lockAndDataForMainnet.address);
-    console.log("lockAndDataForMainnetBefore", lockAndDataBalanceBefore);
+    // add wei to contract throught receiveEth because receiveEth have `payable` parameter
     await lockAndDataForMainnet
       .receiveEth(invoker, {value: addWeiToContract, from: deployer});
-    const res = await lockAndDataForMainnet
+    // execution
+    await lockAndDataForMainnet
       .sendEth(invoker, sendWeiFromContract,
         {from: deployer});
     const contractBalance = await web3.eth.getBalance(lockAndDataForMainnet.address);
+    // expectation
     expect(parseInt(contractBalance, 10))
       .to.equal(parseInt(addWeiToContract, 10) - sendWeiFromContract);
+  });
+
+  it("should work `approveTransfer`", async () => {
+    // preparation
+    const addWeiToContract = "1000";
+    const sendWeiFromContract = 100;
+    // add wei to contract throught receiveEth because receiveEth have `payable` parameter
+    await lockAndDataForMainnet
+      .receiveEth(invoker, {value: addWeiToContract, from: deployer});
+    // execute
+    await lockAndDataForMainnet
+      .approveTransfer(deployer, sendWeiFromContract, {from: deployer});
+    // get value from mapping `approveTransfers`
+    const bn = new BigNumber(await lockAndDataForMainnet.approveTransfers(deployer));
+    // expectation
+    parseInt(bn.toString(), 10).should.be.equal(sendWeiFromContract);
+  });
+
+  it("should work `getMyEth`", async () => {
+    // preparation
+    const addWeiToContract = "1000";
+    const sendWeiFromContract = 100;
+    // add wei to contract throught receiveEth because receiveEth have `payable` parameter
+    await lockAndDataForMainnet
+      .receiveEth(invoker, {value: addWeiToContract, from: deployer});
+    // without `approveTransfer` `getMyEth` not invoke
+    await lockAndDataForMainnet
+      .approveTransfer(deployer, sendWeiFromContract, {from: deployer});
+    // execute
+    await lockAndDataForMainnet
+      .getMyEth({from: deployer});
+    const contractBalance = await web3.eth.getBalance(lockAndDataForMainnet.address);
+    // expectation
+    expect(parseInt(contractBalance, 10))
+      .to.equal(parseInt(addWeiToContract, 10) - sendWeiFromContract);
+  });
+
+  it("should rejected with `User has insufficient ETH` when invoke `getMyEth`", async () => {
+    // preparation
+    const error = "User has insufficient ETH";
+    // execution/expectation
+    await lockAndDataForMainnet
+      .getMyEth({from: deployer})
+      .should.be.eventually.rejectedWith(error);
   });
 
   // it("should invoke sendEth", async () => {
