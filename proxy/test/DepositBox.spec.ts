beb8a1aad6c5885ec3860a10b7d6a8947ee9e22f
elvisgiv <alexeymaratovich@gmail.com>
2019-07-22 17:13:56 +0300

    SKALE-1301 Add tests for DepositBox.sol

--- proxy/test/DepositBox.spec.ts
+++ proxy/test/DepositBox.spec.ts
@@ -31,42 +31,77 @@ contract("DepositBox", ([deployer, user]) => {
        {from: deployer, gas: 8000000 * gasMultiplier});
   });
 
-  // it("should rejected with `Incorrect sender` when invoke postMessage", async () => {
+  it("should rejected with `Unconnected chain` when invoke `deposit`", async () => {
+    // preparation
+    const error = "Unconnected chain";
+    const schainID = "someName";
+    // execution/expectation
+    await depositBox
+      .deposit(schainID, user, {from: deployer})
+      .should.be.eventually.rejectedWith(error);
+  });
+
+  it("should rejected with `SKALE chain name is incorrect` when invoke `deposit`", async () => {
+    // preparation
+    const error = "SKALE chain name is incorrect";
+    const schainID = "Mainnet";
+    // execution/expectation
+    await depositBox
+      .deposit(schainID, user, {from: deployer})
+      .should.be.eventually.rejectedWith(error);
+  });
+
+  it("should rejected with `Not enough money` when invoke `deposit`", async () => {
+    // preparation
+    const schainID = "someName";
+    const error = "Not enough money";
+    // the wei for this error should be LESS than (55000 * 1000000000)
+    // GAS_AMOUNT_POST_MESSAGE * AVERAGE_TX_PRICE constants in DepositBox.sol
+    const wei = "10000";
+    // add schain
+    await lockAndDataForMainnet
+      .addSchain(schainID, deployer, {from: deployer});
+    // execution/expectation
+    await depositBox
+      .deposit(schainID, user, {value: wei, from: deployer})
+      .should.be.eventually.rejectedWith(error);
+  });
+
+  it("should invoke `deposit` without mistakes", async () => {
+    // preparation
+    const schainID = "someAnotherName";
+    // the wei should be MORE than (55000 * 1000000000)
+    // GAS_AMOUNT_POST_MESSAGE * AVERAGE_TX_PRICE constants in DepositBox.sol
+    const wei = "900000000000000";
+    const publicKeyArray = [
+      "1122334455667788990011223344556677889900112233445566778899001122",
+      "1122334455667788990011223344556677889900112233445566778899001122",
+      "1122334455667788990011223344556677889900112233445566778899001122",
+      "1122334455667788990011223344556677889900112233445566778899001122",
+    ];
+    // add schain
+    const chain = await lockAndDataForMainnet
+      .addSchain(schainID, deployer, {from: deployer});
+    // add connected chain
+    await messageProxy
+      .addConnectedChain(schainID, publicKeyArray, {from: deployer});
+    // set contract before invoke `deposit`
+    await lockAndDataForMainnet
+      .setContract("DepositBox", depositBox.address, {from: deployer});
+    // execution
+    console.log("before `deposit` invoke");
+    await depositBox
+      .deposit(schainID, deployer, {value: wei, from: deployer});
+    const lockAndDataBalance = await web3.eth.getBalance(lockAndDataForMainnet.address);
+    // expectation
+    expect(lockAndDataBalance).to.equal(wei);
+  });
+
+  // it("should rejected with `Incorrect sender` when invoke `postMessage`", async () => {
   //   const error = "Incorrect sender";
   //   await depositBox
   //     .postMessage(deployer, "vasya", user, 10, "0x0",
   //     {from: deployer}).should.be.eventually.rejectedWith(error);
-  // });
-
-  // it("should ...", async () => {
-  //   const blabla = await depositBox
-  //     .postMessage(messageProxy.address, "vasya", deployer, 10, "0x0",
-  //     {from: deployer});
-  //   console.log("blabla", blabla);
-  // });
-
-  // it("should ...", async () => {
-  //   const userBalance = await web3.eth.getBalance(user);
-  //   const deployerBalance = await web3.eth.getBalance(deployer);
-  //   const lockAndDataBalance = await web3.eth.getBalance(lockAndDataForMainnet.address);
-  //   //
-  //   console.log("proxyAddress", await depositBox.proxyAddress());
-  //   console.log("messageProxy", messageProxy.address);
-  //   console.log("lockAndDataForMainnet", lockAndDataForMainnet.address);
-  //   console.log("LockAndDataForMainnet", LockAndDataForMainnet.address);
-  //   console.log("deployerBalance", await web3.utils.fromWei(deployerBalance));
-  //   console.log("userBalance", await web3.utils.fromWei(userBalance));
-  //   console.log("lockAndDataBalance", await web3.utils.fromWei(lockAndDataBalance));
-  //   // send wei to contract from deployer
-  //   const sendBabki = await web3.eth
-  //   .sendTransaction({
-  //     from: deployer,
-  //     to: lockAndDataForMainnet.address,
-  //     value: 1000,
-  //     gas: 8000000 * gasMultiplier});
-  //   const contractBalanceAfter = await web3.eth.getBalance(lockAndDataForMainnet.address);
-  //   console.log("contractBalanceAfter", await web3.utils.fromWei(contractBalanceAfter));
-
   // });
 
 });
