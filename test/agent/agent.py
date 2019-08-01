a1f7156a47efa41450ed83f5e91c5f67acf58aad
Dmytro Stebaiev <dmytro@skalelabs.com>
2019-08-01 14:50:51 +0300

    SKALE-1354 Add agent loop

--- test/agent/agent.py
+++ test/agent/agent.py
@@ -1,7 +1,10 @@
 from utils import execute
+from subprocess import Popen
 
 class Agent:
     config = None
+    started = False
+    agent_service = None
 
     def __init__(self, config):
         self.config = config
@@ -9,16 +12,35 @@ class Agent:
     def register(self):
         self._execute_command('register')
 
+    def start(self):
+        if self.agent_service is None:
+            self.agent_service = Popen(self._construct_command('loop'))
+            print(f'Agent process #{self.agent_service.pid}')
+
+    def stop(self):
+        if self.agent_service is not None:
+            self.agent_service.terminate()
+            self.agent_service = None
+
+
     # private
 
     def _execute_command(self, command):
-        execute(f'node {self.config.agent_root}/main.js --verbose=9 \
-            --{command} \
-            --url-main-net={self.config.mainnet_rpc_url} \
-            --url-s-chain={self.config.schain_rpc_url} \
-            --id-main-net=Mainnet \
-            --id-s-chain={self.config.schain_name} \
-            --abi-main-net={self.config.proxy_root}/data/proxyMainnet.json \
-            --abi-s-chain={self.config.proxy_root}/data/proxySchain_{self.config.schain_name}.json \
-            --key-main-net={self.config.mainnet_key} \
-            --key-s-chain={self.config.schain_key}')
+        execute(self._format_command(command))
+
+    def _construct_command(self, command):
+        return ['node',
+                f'{self.config.agent_root}/main.js',
+                '--verbose=9',
+                f'--{command}',
+                f'--url-main-net={self.config.mainnet_rpc_url}',
+                f'--url-s-chain={self.config.schain_rpc_url}',
+                f'--id-main-net=Mainnet',
+                f'--id-s-chain={self.config.schain_name}',
+                f'--abi-main-net={self.config.proxy_root}/data/proxyMainnet.json',
+                f'--abi-s-chain={self.config.proxy_root}/data/proxySchain_{self.config.schain_name}.json',
+                f'--key-main-net={self.config.mainnet_key}',
+                f'--key-s-chain={self.config.schain_key}']
+
+    def _format_command(self, command):
+        return ' '.join(self._construct_command(command))
--- test/test.py
+++ test/test.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import time
 
 from config import Config
 from agent.agent_environment import AgentEnvironment
@@ -15,11 +16,21 @@ def main():
 
     config = Config(src_root, 'config.json')
 
+    # proxyEnvironment = ProxyEnvironment(config)
+    # proxyEnvironment.prepare()
+    #
+    # agentEnvironment = AgentEnvironment(config)
+    # agentEnvironment.prepare()
+
     deployer = Deployer(config)
     deployer.deploy()
 
     agent = Agent(config)
     agent.register()
+
+    agent.start()
+
+    time.sleep(10)
 
 
 if __name__ == '__main__':
