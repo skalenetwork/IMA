50f3fad023aeab0f84eb6f4043ca3ae261d3fd5a
elvisgiv <alexeymaratovich@gmail.com>
2019-08-09 11:10:39 +0300

    SKALE-1299 Add tests for TokenFactory.sol

--- proxy/test/TokenFactory.spec.ts
+++ proxy/test/TokenFactory.spec.ts
@@ -9,6 +9,8 @@ import {
     ERC20OnChainInstance,
     ERC721ModuleForSchainContract,
     ERC721ModuleForSchainInstance,
+    ERC721OnChainContract,
+    ERC721OnChainInstance,
     LockAndDataForSchainContract,
     LockAndDataForSchainERC20Contract,
     LockAndDataForSchainERC20Instance,
@@ -39,6 +41,7 @@ const LockAndDataForSchainERC721: LockAndDataForSchainERC721Contract =
     artifacts.require("./LockAndDataForSchainERC721");
 const ERC721ModuleForSchain: ERC721ModuleForSchainContract = artifacts.require("./ERC721ModuleForSchain");
 const ERC20OnChain: ERC20OnChainContract = artifacts.require("./ERC20OnChain");
+const ERC721OnChain: ERC721OnChainContract = artifacts.require("./ERC721OnChain");
 
 contract("TokenFactory", ([user, deployer]) => {
   let messageProxy: MessageProxyInstance;
@@ -192,6 +195,63 @@ contract("ERC20OnChain", ([user, deployer]) => {
     // expectation
     const balance = await eRC20OnChain.balanceOf(account);
     parseInt(new BigNumber(balance).toString(), 10).should.be.equal(mintAmount - amount);
+  });
+});
+
+contract("ERC721OnChain", ([user, deployer]) => {
+  let messageProxy: MessageProxyInstance;
+  let lockAndDataForSchain: LockAndDataForSchainInstance;
+  let eRC721OnChain: ERC721OnChainInstance;
+
+  beforeEach(async () => {
+    messageProxy = await MessageProxy.new("Mainnet", {from: deployer, gas: 8000000 * gasMultiplier});
+    lockAndDataForSchain = await LockAndDataForSchain.new({from: deployer, gas: 8000000 * gasMultiplier});
+    eRC721OnChain = await ERC721OnChain.new("ERC721OnChain", "ERC721", {from: deployer});
+  });
+
+  it("should invoke `mint`", async () => {
+    // preparation
+    const account = user;
+    const tokenId = 500;
+    // execution
+    await eRC721OnChain.mint(account, tokenId, {from: deployer});
+    // expectation
+    expect(await eRC721OnChain.ownerOf(tokenId)).to.be.equal(account);
+  });
+
+  it("should invoke `burn`", async () => {
+    // preparation
+    const error = "ERC721: owner query for nonexistent token";
+    const tokenId = 55;
+    // mint to avoid `owner query for nonexistent token` error
+    await eRC721OnChain.mint(deployer, tokenId, {from: deployer});
+    // execution
+    await eRC721OnChain.burn(tokenId, {from: deployer});
+    // expectation
+    await eRC721OnChain.ownerOf(tokenId).should.be.eventually.rejectedWith(error);
+  });
+
+  it("should reject with `ERC721Burnable: caller is not owner nor approved` when invoke `burn`", async () => {
+    // preparation
+    const error = "ERC721Burnable: caller is not owner nor approved";
+    const tokenId = 55;
+    const account = user;
+    // mint to avoid `owner query for nonexistent token` error
+    await eRC721OnChain.mint(deployer, tokenId, {from: deployer});
+    // execution/expectation
+    await eRC721OnChain.burn(tokenId, {from: account}).should.be.eventually.rejectedWith(error);
+  });
+
+  it("should invoke `setTokenURI`", async () => {
+    // preparation
+    const tokenURI = "Some string with describe token";
+    const tokenId = 55;
+    // mint to avoid `owner query for nonexistent token` error
+    await eRC721OnChain.mint(deployer, tokenId, {from: deployer});
+    // execution
+    const res = await eRC721OnChain.setTokenURI(tokenId, tokenURI, {from: deployer});
+    // expectation
+    expect(res.receipt.status).to.be.true;
   });
 
 });
