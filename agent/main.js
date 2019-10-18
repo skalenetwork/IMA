471261fdbaf3a390da4eb5f7a032488c3d4f8cf5
Sergiy Lavrynenko <sergiy@skalelabs.com>
2019-10-18 16:52:52 +0300

    SKALE-1613 new binary message glue to signe messages in a more compatible way with Soliditys abi.encodePacked() API

--- agent/main.js
+++ agent/main.js
@@ -1659,6 +1659,34 @@ if ( g_bShowConfigMode ) {
     return true;
 }
 
+
+
+// console.log( hexToBytes("0x7aa5E36AA15E93D10F4F26357C30F052DacDde5F") );
+// console.log( bytesToHex( hexToBytes("0x7aa5E36AA15E93D10F4F26357C30F052DacDde5F") ) );
+// return;
+
+
+// let joMessage =
+// {
+//     "amount": "1000000000000000000",
+//     "data": "0x01",
+//     "destinationContract": "0x88a5eDcf315599Ade5b6b4cC0991A23Bf9E88f65",
+//     "sender": "0xc2fe505C79C82bb8CEf48709816480ff6E1E0379",
+//     "to": "0x7aa5E36AA15E93D10F4F26357C30F052DacDde5F"
+// };
+// // {
+// //     "amount": "1000000000000000000",
+// //     "data": "0x01",
+// //     "destinationContract": "0x88a5eDcf315599Ade5b6b4cC0991A23Bf9E88f65",
+// //     "sender": "0xc2fe505C79C82bb8CEf48709816480ff6E1E0379",
+// //     "to": "0x7aa5E36AA15E93D10F4F26357C30F052DacDde5F"
+// // };
+// console.log( joMessage );
+// console.log( compose_one_message_byte_sequence( joMessage ).length );
+// console.log( compose_one_message_byte_sequence( joMessage ) );
+// console.log( bytesToHex( compose_one_message_byte_sequence( joMessage ) ) );
+// return;
+
 async function discover_s_chain_network( fnAfter ) {
     fnAfter = fnAfter || function() {};
     let joSChainNetworkInfo = null;
@@ -1985,22 +2013,210 @@ function discover_common_public_key( joSChainNetworkInfo ) {
     return null;
 }
 
+function encodeUTF8( s ) { // marshals a string to an Uint8Array, see https://gist.github.com/pascaldekloe/62546103a1576803dade9269ccf76330
+	var i = 0, arrBytes = new Uint8Array(s.length * 4);
+	for( var ci = 0; ci != s.length; ci++ ) {
+		var c = s.charCodeAt( ci );
+		if( c < 128 ) {
+			arrBytes[i++] = c;
+			continue;
+		}
+		if( c < 2048 )
+			arrBytes[i++] = c >> 6 | 192;
+		else {
+			if( c > 0xd7ff && c < 0xdc00 ) {
+				if( ++ci >= s.length )
+					throw new Error( "UTF-8 encode: incomplete surrogate pair" );
+				var c2 = s.charCodeAt( ci );
+				if( c2 < 0xdc00 || c2 > 0xdfff )
+					throw new Error( "UTF-8 encode: second surrogate character 0x" + c2.toString(16) + " at index " + ci + " out of range" );
+				c = 0x10000 + ((c & 0x03ff) << 10) + (c2 & 0x03ff);
+				arrBytes[i++] = c >> 18 | 240;
+				arrBytes[i++] = c >> 12 & 63 | 128;
+			} else arrBytes[i++] = c >> 12 | 224;
+			arrBytes[i++] = c >> 6 & 63 | 128;
+		}
+		arrBytes[i++] = c & 63 | 128;
+	}
+	return arrBytes.subarray( 0, i );
+}
+function decodeUTF8( arrBytes ) { // unmarshals a string from an Uint8Array, see https://gist.github.com/pascaldekloe/62546103a1576803dade9269ccf76330
+	var i = 0, s = "";
+	while ( i < arrBytes.length ) {
+		var c = arrBytes[i++];
+		if( c > 127 ) {
+			if( c > 191 && c < 224 ) {
+				if( i >= arrBytes.length )
+					throw new Error( "UTF-8 decode: incomplete 2-byte sequence" );
+				c = (c & 31) << 6 | arrBytes[i++] & 63;
+			} else if( c > 223 && c < 240 ) {
+				if (i + 1 >= arrBytes.length)
+					throw new Error( "UTF-8 decode: incomplete 3-byte sequence" );
+				c = (c & 15) << 12 | (arrBytes[i++] & 63) << 6 | arrBytes[i++] & 63;
+			} else if( c > 239 && c < 248 ) {
+				if( i + 2 >= arrBytes.length )
+					throw new Error( "UTF-8 decode: incomplete 4-byte sequence" );
+				c = (c & 7) << 18 | (arrBytes[i++] & 63) << 12 | (arrBytes[i++] & 63) << 6 | arrBytes[i++] & 63;
+			} else
+                throw new Error( "UTF-8 decode: unknown multibyte start 0x" + c.toString(16) + " at index " + (i - 1) );
+		}
+		if( c <= 0xffff )
+            s += String.fromCharCode( c );
+		else if( c <= 0x10ffff ) {
+			c -= 0x10000;
+			s += String.fromCharCode( c >> 10 | 0xd800 )
+			s += String.fromCharCode( c & 0x3FF | 0xdc00 )
+		} else
+            throw new Error( "UTF-8 decode: code point 0x" + c.toString(16) + " exceeds UTF-16 reach" );
+	}
+	return s;
+}
+
+function hexToBytes( strHex, isInversiveOrder ) { // convert a hex string to a byte array
+    isInversiveOrder = ( isInversiveOrder != null && isInversiveOrder != undefined && isInversiveOrder ) ? true : false;
+    strHex = strHex || "";
+    strHex = "" + strHex;
+    strHex = strHex.trim().toLowerCase();
+    if( strHex.length > 1 && strHex[0] == 0 && strHex[1] == "x" )
+        strHex = strHex.substr( 2, strHex.length - 2 );
+    if( ( strHex.length & 1 ) != 0 )
+        strHex = "0" + strHex;
+//console.log( "    ", "strHex", " txt--> ", strHex );
+    let i, j, cnt = strHex.length;
+    let arrBytes = new Uint8Array( cnt/2 );
+    for( i = 0, j = 0; i < cnt; ++ j, i += 2 )
+        arrBytes[ isInversiveOrder ? ( cnt - j - 1 ) : j ] = parseInt( strHex.substr( i, 2 ), 16 );
+//console.log( "    ", strHex, " --> ", bytesToHex(arrBytes) );
+//console.log( "    ", strHex, " --> ", arrBytes.join(" ") );
+    return arrBytes;
+}
+function bytesToHex( arrBytes, isInversiveOrder ) { // convert a byte array to a hex string
+    isInversiveOrder = ( isInversiveOrder != null && isInversiveOrder != undefined && isInversiveOrder ) ? true : false;
+    for( var hex = [], i = 0; i < arrBytes.length; i++)  {
+        var current = arrBytes[i] < 0 ? arrBytes[i] + 256 : arrBytes[i];
+        let c0 = ( current >>> 4 ).toString(16);
+        let c1 = ( current & 0xF ).toString(16);
+        if( isInversiveOrder ) {
+            hex.splice( 0, 0, c0 );
+            hex.splice( 1, 0, c1 );
+        } else {
+            hex.push( c0 );
+            hex.push( c1 );
+        }
+    }
+    return hex.join( "" );
+}
+
+function bytesAlighLeftWithZeroes( arrBytes, cntMin ) {
+    let arrOneZeroByte = new Uint8Array( 1 );
+    arrOneZeroByte[0] = 0;
+    while( arrBytes.length < cntMin )
+        arrBytes = bytesConcat( arrOneZeroByte, arrBytes );
+    return arrBytes;
+}
+function bytesAlighRightWithZeroes( arrBytes, cntMin ) {
+    let arrOneZeroByte = new Uint8Array( 1 );
+    arrOneZeroByte[0] = 0;
+    while( arrBytes.length < cntMin )
+        arrBytes = bytesConcat( arrBytes, arrOneZeroByte );
+    return arrBytes;
+}
+
+function concatTypedArrays( a, b ) { // a, b TypedArray of same type
+    var c = new (a.constructor)(a.length + b.length);
+    c.set( a, 0 );
+    c.set( b, a.length );
+    return c;
+}
+function concatByte( ui8a, byte ) {
+    var b = new Uint8Array( 1 );
+    b[0] = byte;
+    return concatTypedArrays(ui8a, b);
+}
+function bytesConcat( a1, a2 ) {
+    a1 = a1 || new Uint8Array();
+    a2 = a2 || new Uint8Array();
+    return concatTypedArrays( a1, a2 );
+}
+
+function toArrayBuffer( buf ) { // see https://stackoverflow.com/questions/8609289/convert-a-binary-nodejs-buffer-to-javascript-arraybuffer
+    let ab = new ArrayBuffer( buf.length );
+    let view = new Uint8Array( ab );
+    for( var i = 0; i < buf.length; ++ i )
+        view[ i ] = buf[ i ];
+    return ab;
+}
+function toBuffer( ab ) {
+    let buf = Buffer.alloc( ab.byteLength );
+    let view = new Uint8Array( ab );
+    for( let i = 0; i < buf.length; ++ i )
+        buf[ i ] = view[ i ];
+    return buf;
+}
+
+function invertArrayItemsLR( arr ) {
+    let i, cnt = arr.length / 2;
+    for( i = 0; i < cnt; ++ i ) {
+        let e1 = arr[ i ];
+        let e2 = arr[ arr.length - i - 1 ];
+        arr[ i ] = e2;
+        arr[ arr.length - i - 1 ] = e1;
+    }
+    return arr;
+}
+
+function compose_one_message_byte_sequence( joMessage ) {
+    let w3 = g_w3_s_chain ? g_w3_s_chain : g_w3_main_net;
+    if( ! w3 )
+        throw new Error( "w3.utils is needed for BN operations" );
+    let arrBytes = new Uint8Array();
+
+    let bytesSender = hexToBytes( joMessage.sender );
+    bytesSender = bytesAlighLeftWithZeroes( bytesSender, 32 )
+    bytesSender = invertArrayItemsLR( bytesSender );
+    arrBytes = bytesConcat( arrBytes, bytesSender );
+    //
+    let bytesDestinationContract = hexToBytes( joMessage.destinationContract );
+    bytesDestinationContract = bytesAlighLeftWithZeroes( bytesDestinationContract, 32 )
+    bytesDestinationContract = invertArrayItemsLR( bytesDestinationContract );
+    arrBytes = bytesConcat( arrBytes, bytesDestinationContract );
+    //
+    let bytesTo = hexToBytes( joMessage.to );
+    bytesTo = bytesAlighLeftWithZeroes( bytesTo, 32 )
+    bytesTo = invertArrayItemsLR( bytesTo );
+    arrBytes = bytesConcat( arrBytes, bytesTo );
+    //
+    let strHexAmount = "0x" + w3.utils.toBN( joMessage.amount ).toString(16);
+console.log( "strHexAmount", strHexAmount );
+    let bytesAmount = hexToBytes( strHexAmount );
+console.log( "bytesAmount1", bytesToHex(bytesAmount) );
+    bytesAmount = invertArrayItemsLR( bytesAmount );
+console.log( "bytesAmount2", bytesToHex(bytesAmount) );
+    bytesAmount = bytesAlighRightWithZeroes( bytesAmount, 32 )
+    arrBytes = bytesConcat( arrBytes, bytesAmount );
+    //
+    let bytesData = hexToBytes( joMessage.data );
+    arrBytes = bytesConcat( arrBytes, bytesData );
+    bytesData = invertArrayItemsLR( bytesData );
+    //
+    return arrBytes;
+}
+
 function compose_summary_message_to_sign( jarrMessages, isHash ) {
-    //return "1122334455";
-
-
-    let strSummaryMessage = "";
+    let arrBytes = "";
     let i = 0, cnt = jarrMessages.length;
     for( i = 0; i < cnt; ++ i ) {
-        let joMessagge = jarrMessages[ i ];
-        strSummaryMessage += joMessagge.data;
+        let joMessage = jarrMessages[ i ];
+        let arrMessageBytes = compose_one_message_byte_sequence( joMessage );
+        arrBytes = bytesConcat( arrBytes, arrMessageBytes );
     }
+    let strSummaryMessage = "";
     if( isHash ) {
         const hash = new Keccak( 256 );
-        hash.update( strSummaryMessage  );
+        hash.update( toBuffer( arrBytes ) );
         strSummaryMessage = hash.digest( "hex" );
-    }
-
+    } else
+        strSummaryMessage = "0x" + bytesToHex( arrBytes );
     return strSummaryMessage;
 }
 
@@ -2029,6 +2245,8 @@ function perform_bls_glue( jarrMessages, arrSignResults ) {
     let jarrNodes = g_joSChainNetworkInfo.network;
     let nThreshold = discover_bls_threshold( g_joSChainNetworkInfo );
     let nParticipants = discover_bls_participants( g_joSChainNetworkInfo );
+    //if ( IMA.verbose_get() >= IMA.RV_VERBOSE.info )
+       log.write( cc.debug( "Original long message is ") + cc.info( compose_summary_message_to_sign( jarrMessages, false ) ) + "\n" );
     let strSummaryMessage = compose_summary_message_to_sign( jarrMessages, true );
     let strPWD = shell.pwd();
     let strActionDir = alloc_tmp_action_dir();
