5992b0145fcdb744120b33e217ed6067e3c9ce72
Artem Payvin <payvinartem@gmail.com>
2019-10-21 18:44:56 +0300

    Changed skale-manager branch

--- proxy/scripts/prepare.sh
+++ proxy/scripts/prepare.sh
@@ -3,7 +3,7 @@
 export $(cat .env | xargs)
 cd data
 sudo apt-get -y install jq
-git clone -b stable https://${GITHUB_TOKEN}\@github.com/skalenetwork/skale-manager.git &&
+git clone -b enhancement/Upgrade-SkaleVerifier https://${GITHUB_TOKEN}\@github.com/skalenetwork/skale-manager.git &&
 cd skale-manager &&
 npm install &&
 #PRIVATE_KEY=${MNEMONIC_FOR_MAINNET} ENDPOINT=${MAINNET_RPC_URL} ./node_modules/.bin/truffle migrate --network unique
