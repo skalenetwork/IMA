bb55f23c6c77ead347f3d3f73c41b7bc2e06621d
Artem Payvin <payvinartem@gmail.com>
2019-10-23 16:15:36 +0300

    SKALE-1213 remove previous changes

--- proxy/scripts/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-./node_modules/.bin/truffle compile
-typechain --target truffle './build/**/*.json'
-# if [ -f "./test/utils/typings.d.ts" ]; then
-#     mv ./test/utils/typings.d.ts ./types/truffle-contracts/.
-# fi
-
-exit
