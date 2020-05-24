248688723b48a3a8e835662b132cbea218b834a1
Chadwick Strange <cstrangedk@users.noreply.github.com>
2020-05-24 11:54:44 +0200

    Correct license headers

--- npms/skale-owasp/owasp-util.js
+++ npms/skale-owasp/owasp-util.js
@@ -1,4 +1,29 @@
 
+// SPDX-License-Identifier: AGPL-3.0-only
+
+/**
+ * @license
+ * SKALE IMA
+ *
+ * SKALE IMA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SKALE IMA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SKALE IMA.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file owasp-util.js
+ * @copyright SKALE Labs 2019-Present
+ */
+
 // introduction: https://github.com/Checkmarx/JS-SCP
 // main PDF with rules to follow: https://www.gitbook.com/download/pdf/book/checkmarx/JS-SCP
 // top 10 hit parade: https://owasp.org/www-project-top-ten/
--- proxy/scripts/prepare.sh
+++ proxy/scripts/prepare.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 export $(cat .env | xargs)
-cd data
+cd data || { echo "Failure: data directory not found"; exit 1; }
 sudo apt-get -y install jq
 git clone -b enhancement/Upgrade-SkaleVerifier https://${GITHUB_TOKEN}\@github.com/skalenetwork/skale-manager.git &&
 cd skale-manager &&
 npm install &&
 #PRIVATE_KEY=${MNEMONIC_FOR_MAINNET} ENDPOINT=${MAINNET_RPC_URL} ./node_modules/.bin/truffle migrate --network unique
 ./node_modules/.bin/truffle migrate --network test
-cd data 
+cd data || { echo "Failure: data directory not found"; exit 1; }
 export CONTRACT_MANAGER_ADDRESS=$( jq .contract_manager_address test.json )
 cp test.json ../../.
 cd ../../
