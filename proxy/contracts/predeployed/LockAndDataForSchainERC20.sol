1036dd2c834690dcfd9576960613d8b38053b4e7
Chadwick Strange <cstrangedk@users.noreply.github.com>
2020-11-06 12:39:50 +0100

    Add docs to LockAndDataForSchainERC20

--- proxy/contracts/predeployed/LockAndDataForSchainERC20.sol
+++ proxy/contracts/predeployed/LockAndDataForSchainERC20.sol
@@ -29,10 +29,21 @@ interface ERC20MintAndBurn {
     function burn(uint256 amount) external;
 }
 
-
+/**
+ * @title Lock and Data For SKALE chain ERC20
+ * @dev Runs on SKALE chains, holds deposited ERC20s, and contains mappings and
+ * balances of ERC20 tokens received through DepositBox.
+ */
 contract LockAndDataForSchainERC20 is PermissionsForSchain {
 
-    event SendedERC20(bool result);
+    /**
+     * @dev Emitted upon minting on the SKALE chain.
+     */
+    event SentERC20(bool result);
+    
+    /**
+     * @dev Emitted upon receipt in LockAndDataForSchainERC20.
+     */
     event ReceivedERC20(bool result);
 
     mapping(uint256 => address) public erc20Tokens;
@@ -43,12 +54,27 @@ contract LockAndDataForSchainERC20 is PermissionsForSchain {
         // solium-disable-previous-line no-empty-blocks
     }
 
+    /**
+     * @dev Allows ERC20Module to send (mint) ERC20 tokens to LockAndDataForSchainERC20.
+     * 
+     * Emits a {SentERC20} event.
+     */
     function sendERC20(address contractHere, address to, uint256 amount) external allow("ERC20Module") returns (bool) {
         ERC20MintAndBurn(contractHere).mint(to, amount);
         emit SendedERC20(true);
         return true;
     }
 
+    /**
+     * @dev Allows ERC20Module to receive ERC20 tokens to LockAndDataForSchainERC20.
+     * 
+     * Emits a {ReceivedERC20} event.
+     * 
+     * Requirements:
+     * 
+     * - `amount` must be less than or equal to the balance in
+     * LockAndDataForSchainERC20.
+     */
     function receiveERC20(address contractHere, uint256 amount) external allow("ERC20Module") returns (bool) {
         require(ERC20MintAndBurn(contractHere).balanceOf(address(this)) >= amount, "Amount not transfered");
         ERC20MintAndBurn(contractHere).burn(amount);
@@ -56,6 +82,9 @@ contract LockAndDataForSchainERC20 is PermissionsForSchain {
         return true;
     }
 
+    /**
+     * @dev Allows ERC20Module to add an ERC20 token to LockAndDataForSchainERC20.
+     */
     function addERC20Token(address addressERC20, uint256 contractPosition) external allow("ERC20Module") {
         erc20Tokens[contractPosition] = addressERC20;
         erc20Mapper[addressERC20] = contractPosition;
