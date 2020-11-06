a41615e55dc82cb5095df3bbbed79c08c579bdc7
Dmytro <dmitry.tkk@gmail.com>
2020-11-06 13:03:16 +0200

    SKALE-2001 Add registration checks

--- agent/run.sh
+++ agent/run.sh
@@ -97,10 +97,18 @@ BASE_OPTIONS="--gas-price-multiplier=$GAS_PRICE_MULTIPLIER \
 echo Base options:
 echo $BASE_OPTIONS
 
-node $DIR/main.js --help
+echo Going to run: node $DIR/main.js --check-registration $BASE_OPTIONS
+node $DIR/main.js --check-registration $BASE_OPTIONS
 
-echo Going to run: node $DIR/main.js --register $BASE_OPTIONS
-node $DIR/main.js --register $BASE_OPTIONS
+if [ $? -eq 0 ]
+then
+    echo "IMA is not registered yet"
+    echo Going to run: node $DIR/main.js --register $BASE_OPTIONS
+    node $DIR/main.js --register $BASE_OPTIONS
+else
+  echo "IMA is already registered"
+fi
 
+echo "Running loop cmd..."
 echo Going to run: node $DIR/main.js --loop $BASE_OPTIONS
 node $DIR/main.js --loop $BASE_OPTIONS
