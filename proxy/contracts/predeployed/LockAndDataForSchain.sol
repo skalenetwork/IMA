9bec87b756b2e2c2f483f5bc53e002ebc901917c
Chadwick Strange <cstrangedk@users.noreply.github.com>
2020-11-06 12:59:21 +0100

    Add docs to LockAndDataForSchain

--- proxy/contracts/predeployed/LockAndDataForSchain.sol
+++ proxy/contracts/predeployed/LockAndDataForSchain.sol
@@ -30,7 +30,11 @@ interface IETHERC20 {
     function burnFrom(address from, uint256 amount) external;
 }
 
-
+/**
+ * @title Lock and Data For SKALE chain
+ * @dev Runs on SKALE chains, holds deposited ETH, and contains mappings and
+ * balances of ETH tokens received through DepositBox.
+ */
 contract LockAndDataForSchain is OwnableForSchain {
 
     address private ethERC20Address_; // l_sergiy: changed name _
@@ -58,10 +62,22 @@ contract LockAndDataForSchain is OwnableForSchain {
         authorizedCaller[msg.sender] = true;
     }
 
+    /**
+     * @dev Allows Owner to set a EthERC20 contract address.
+     */
     function setEthERC20Address(address newEthERC20Address) external onlyOwner {
         ethERC20Address_ = newEthERC20Address;
     }
 
+    /**
+     * @dev Allows Owner to set a new contract address.
+     *
+     * Requirements:
+     *
+     * - New contract address must be non-zero.
+     * - New contract address must not already be added.
+     * - Contract must contain code.
+     */
     function setContract(string calldata contractName, address newContract) external onlyOwner {
         require(newContract != address(0), "New address is equal zero");
 
@@ -78,6 +94,9 @@ contract LockAndDataForSchain is OwnableForSchain {
         permitted[contractId] = newContract;
     }
 
+    /**
+     * @dev Checks whether LockAndDataForSchain is connected to a SKALE chain.
+     */
     function hasSchain( string calldata schainID ) external view returns (bool) {
         bytes32 schainHash = keccak256(abi.encodePacked(schainID));
         if ( tokenManagerAddresses[schainHash] == address(0) ) {
@@ -86,6 +105,16 @@ contract LockAndDataForSchain is OwnableForSchain {
         return true;
     }
 
+    /**
+     * @dev Adds a SKALE chain and its TokenManager address to
+     * LockAndDataForSchain.
+     *
+     * Requirements:
+     *
+     * - `msg.sender` must be authorized caller.
+     * - SKALE chain must not already be added.
+     * - TokenManager address must be non-zero.
+     */
     function addSchain(string calldata schainID, address tokenManagerAddress) external {
         require(authorizedCaller[msg.sender] || getOwner() == msg.sender, "Not authorized caller");
         bytes32 schainHash = keccak256(abi.encodePacked(schainID));
@@ -94,12 +123,22 @@ contract LockAndDataForSchain is OwnableForSchain {
         tokenManagerAddresses[schainHash] = tokenManagerAddress;
     }
 
+    /**
+     * @dev Allows Owner to remove a SKALE chain from LockAndDataForSchain.
+     *
+     * Requirements:
+     *
+     * - SKALE chain must already be set.
+     */
     function removeSchain(string calldata schainID) external onlyOwner {
         bytes32 schainHash = keccak256(abi.encodePacked(schainID));
         require(tokenManagerAddresses[schainHash] != address(0), "SKALE chain is not set");
         delete tokenManagerAddresses[schainHash];
     }
 
+    /**
+     * @dev Checks whether LockAndDataForSchain is connected to a DepositBox.
+     */
     function hasDepositBox() external view returns(bool) {
         bytes32 depositBoxHash = keccak256(abi.encodePacked("Mainnet"));
         if ( tokenManagerAddresses[depositBoxHash] == address(0) ) {
@@ -108,6 +147,15 @@ contract LockAndDataForSchain is OwnableForSchain {
         return true;
     }
 
+    /**
+     * @dev Adds a DepositBox address to LockAndDataForSchain.
+     *
+     * Requirements:
+     *
+     * - `msg.sender` must be authorized caller.
+     * - DepositBox must not already be added.
+     * - DepositBox address must be non-zero.
+     */
     function addDepositBox(address depositBoxAddress) external {
         require(authorizedCaller[msg.sender] || getOwner() == msg.sender, "Not authorized caller");
         require(depositBoxAddress != address(0), "Incorrect Deposit Box address");
@@ -122,6 +170,13 @@ contract LockAndDataForSchain is OwnableForSchain {
         ] = depositBoxAddress;
     }
 
+    /**
+     * @dev Allows Owner to remove a DepositBox from LockAndDataForSchain.
+     *
+     * Requirements:
+     *
+     * - DepositBox must already be set.
+     */
     function removeDepositBox() external onlyOwner {
         require(
             tokenManagerAddresses[
@@ -132,18 +187,30 @@ contract LockAndDataForSchain is OwnableForSchain {
         delete tokenManagerAddresses[keccak256(abi.encodePacked("Mainnet"))];
     }
 
+    /**
+     * @dev Allows Owner to add an authorized caller.
+     */
     function addAuthorizedCaller(address caller) external onlyOwner {
         authorizedCaller[caller] = true;
     }
 
+    /**
+     * @dev Allows Owner to remove an authorized caller.
+     */
     function removeAuthorizedCaller(address caller) external onlyOwner {
         authorizedCaller[caller] = false;
     }
 
+    /**
+     * @dev Allows TokenManager to add gas costs to LockAndDataForSchain.
+     */
     function addGasCosts(address to, uint256 amount) external allow("TokenManager") {
         ethCosts[to] += amount;
     }
 
+    /**
+     * @dev Allows TokenManager to reduce gas costs from LockAndDataForSchain.
+     */
     function reduceGasCosts(address to, uint256 amount) external allow("TokenManager") returns (bool) {
         if (ethCosts[to] >= amount) {
             ethCosts[to] -= amount;
@@ -155,21 +222,33 @@ contract LockAndDataForSchain is OwnableForSchain {
         return false;
     }
 
+    /**
+     * @dev Allows TokenManager to remove gas costs from LockAndDataForSchain.
+     */
     function removeGasCosts(address to) external allow("TokenManager") returns (uint256 balance) {
         balance = ethCosts[to];
         delete ethCosts[to];
     }
 
+    /**
+     * @dev Allows TokenManager to send (mint) ETH from LockAndDataForSchain.
+     */
     function sendEth(address to, uint256 amount) external allow("TokenManager") returns (bool) {
         require(IETHERC20(getEthERC20Address()).mint(to, amount), "Mint error");
         return true;
     }
 
+    /**
+     * @dev Allows TokenManager to receive (burn) ETH to LockAndDataForSchain.
+     */
     function receiveEth(address sender, uint256 amount) external allow("TokenManager") returns (bool) {
         IETHERC20(getEthERC20Address()).burnFrom(sender, amount);
         return true;
     }
 
+    /**
+     * @dev Returns EthERC20 contract address.
+     */
     function getEthERC20Address() /*external onlyOwner*/ /*private*/ public view returns ( address a ) {
         if (ethERC20Address_ == address(0) && (!isCustomDeploymentMode_)) {
             return SkaleFeatures(0x00c033b369416c9ecd8e4a07aafa8b06b4107419e2).getConfigVariableAddress("skaleConfig.contractSettings.IMA.ethERC20Address");
@@ -178,6 +257,9 @@ contract LockAndDataForSchain is OwnableForSchain {
     }
 
     // l_sergiy: added checkPermitted() function
+    /**
+     * @dev Checks whether contract name and adress are permitted..
+     */
     function checkPermitted( string memory contractName, address contractAddress ) private view returns ( bool rv ) {
         require(contractAddress != address(0), "contract address required to check permitted status");
         bytes32 contractId = keccak256(abi.encodePacked(contractName));
