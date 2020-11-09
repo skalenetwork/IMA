26b92949de870f6c8db10608551d50656261cf5f
Sergiy Lavrynenko <sergiy@skalelabs.com>
2020-11-09 15:14:42 +0200

    SKALE-3464 removed redundant and test contracts from coverage reports

--- proxy/.solcover.js
+++ proxy/.solcover.js
@@ -2,7 +2,10 @@ module.exports = {
     compileCommand: 'npx truffle compile --network coverage',
     testCommand: 'npx truffle test --network coverage --gas_multiplier 10',    
     norpc: true,
-    skipFiles: [],
+    skipFiles: [
+        "Migrations.sol",
+        "test/LockAndDataForMainnetWorkaround.sol"
+    ],
     providerOptions: {
         default_balance_ether: 3000, //000000000000000000,
         callGasLimit: 10000000,
