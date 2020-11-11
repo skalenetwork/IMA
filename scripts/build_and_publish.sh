aa677dc089362de8002df01edce4852998153e9e
Vadim Yavorsky <yavrsky@gmail.com>
2020-11-11 06:58:14 +0200

    test commit

---
+++ scripts/build_and_publish.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+: "${VERSION?Need to set VERSION}"
+: "${BRANCH?Need to set BRANCH}"
+
+NAME=IMA
+REPO_NAME=skalenetwork/$NAME
+IMAGE_NAME=$REPO_NAME:$VERSION
+LATEST_IMAGE_NAME=$REPO_NAME:$BRANCH-latest
+
+# Build image
+
+echo "Building $IMAGE_NAME..."
+docker build -t $IMAGE_NAME . || exit $?
+docker tag $IMAGE_NAME $LATEST_IMAGE_NAME
+
+echo "========================================================================================="
+echo "Built $IMAGE_NAME"
+
+# Publish image
+
+: "${DOCKER_USERNAME?Need to set DOCKER_USERNAME}"
+: "${DOCKER_PASSWORD?Need to set DOCKER_PASSWORD}"
+
+echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
+
+docker push $IMAGE_NAME || exit $?
+docker push $LATEST_IMAGE_NAME || exit $?
