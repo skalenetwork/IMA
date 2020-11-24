6ad453ab8415223c1a79c5b79235b4d646e04bf8
Sergiy Lavrynenko <sergiy@skalelabs.com>
2020-11-24 13:09:22 +0200

    SKALE-3461 SkaleFeatures fixed, message loop works in built-in IMA mode

--- proxy/contracts/predeployed/TokenManager.sol
+++ proxy/contracts/predeployed/TokenManager.sol
@@ -551,23 +551,6 @@ contract TokenManager is PermissionsForSchain {
         );
     }
 
-    function toAsciiString(address x) public pure returns (string memory) {
-        bytes memory s = new bytes(40);
-        for (uint i = 0; i < 20; i++) {
-            byte b = byte(uint8(uint(x) / (2**(8*(19 - i)))));
-            byte hi = byte(uint8(b) / 16);
-            byte lo = byte(uint8(b) - 16 * uint8(hi));
-            s[2*i] = char(hi);
-            s[2*i+1] = char(lo);            
-        }
-        return string(s);
-    }
-
-    function char(byte b) public pure returns (byte c) {
-        if (uint8(b) < 10) return byte(uint8(b) + 0x30);
-        else return byte(uint8(b) + 0x57);
-    }
-
     /**
      * @dev Converts the first byte of data to an operation.
      * 
