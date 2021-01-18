082b9328196ca57b4987b5d75497a8e5cdd0b430
Chadwick Strange <cstrangedk@users.noreply.github.com>
2021-01-18 12:54:47 -0500

    Fix typos in tests

--- proxy/test/utils/helper.ts
+++ proxy/test/utils/helper.ts
@@ -24,12 +24,12 @@
  */
 
 export function randomString(length: number) {
-    const radom13chars = () => {
+    const random13chars = () => {
         return Math.random().toString(16).substring(2, 15);
     };
     const loops = Math.ceil(length / 13);
-    return new Array(loops).fill(radom13chars).reduce((string, func) => {
-        return string + func();
+    return new Array(loops).fill(random13chars).reduce((stringg, func) => {
+        return stringg + func();
     }, "").substring(0, length);
 }
 
