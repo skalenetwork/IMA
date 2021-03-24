d7c65b2bb27669ec1b71cc29889d9f06d0c8cb10
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-03-24 13:15:45 +0200

    SKALE-3990 Add mocks

---
+++ proxy/contracts/test/ReceiverMock.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+/**
+ *   ReceiverMock.sol - SKALE Interchain Messaging Agent
+ *   Copyright (C) 2021-Present SKALE Labs
+ *   @author Dmytro Stebaiev
+ *
+ *   SKALE IMA is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SKALE IMA is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with SKALE IMA.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "../schain/MessageProxyForSchain.sol";
+
+
+contract ReceiverMock is ContractReceiverForSchain {
+    function postMessage(
+        string calldata,
+        address,
+        address,
+        uint256,
+        bytes calldata
+    )
+        external
+        override
+        returns (bool) 
+    {
+        return true;
+    }
+}
+
+contract ReceiverMockDeployer {
+    address public lastDeployed;
+    function deploy() public returns (address) {
+        lastDeployed = address(new ReceiverMock{salt: bytes32(uint(0xd2))}());
+    }
+}
