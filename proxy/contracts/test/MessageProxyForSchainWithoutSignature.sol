6c58b15c0c851880f56e803a30baace0b551299d
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-04-02 16:22:40 +0300

    SKALE-3990 Fix linter errors

--- proxy/contracts/test/MessageProxyForSchainWithoutSignature.sol
+++ proxy/contracts/test/MessageProxyForSchainWithoutSignature.sol
@@ -26,9 +26,9 @@ import "../schain/MessageProxyForSchain.sol";
 
 contract MessageProxyForSchainWithoutSignature is MessageProxyForSchain {
 
-    constructor(string memory newChainID) public MessageProxyForSchain(newChainID) {
-        
-    }
+    constructor(string memory newChainID) public MessageProxyForSchain(newChainID)
+    // solhint-disable-next-line no-empty-blocks
+    { }
 
     function _verifyMessages(
         Message[] calldata,
