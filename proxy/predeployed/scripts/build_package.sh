3002d85be791cf652cf0bc1129571bcc161b9d0c
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-04-27 16:57:38 +0300

    SKALE-3418 Copy ProxyAdmin artifacts

--- proxy/predeployed/scripts/build_package.sh
+++ proxy/predeployed/scripts/build_package.sh
@@ -3,5 +3,6 @@
 set -e
 
 cd "$(dirname "$0")/.."
-cp ../node_modules/@openzeppelin/upgrades/build/contracts/ProxyAdmin.json src/ima_predeployed/artifacts
+mkdir -p src/ima_predeployed/artifacts/
+cp -v ../node_modules/@openzeppelin/upgrades/build/contracts/ProxyAdmin.json src/ima_predeployed/artifacts/
 python3 -m build
