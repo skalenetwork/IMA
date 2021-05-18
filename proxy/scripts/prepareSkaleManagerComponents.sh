0dbee9cade31161b163a3576df9b7a583452e2ba
Artem Payvin <payvinartem@gmail.com>
2021-05-18 10:03:40 +0300

    SKALE-4120 fix file location error

--- proxy/scripts/prepareSkaleManagerComponents.sh
+++ proxy/scripts/prepareSkaleManagerComponents.sh
@@ -2,8 +2,8 @@
 
 export $(xargs < .env)
 if [ -z "$RUNNING_NETWORK" ]; then
-    npx hardhat run migrations/deploySkaleManagerComponents --network "${NETWORK_FOR_ETHEREUM}"
+    npx hardhat run migrations/deploySkaleManagerComponents.ts --network "${NETWORK_FOR_ETHEREUM}"
 else
-    npx hardhat run migrations/deploySkaleManagerComponents --network "${RUNNING_NETWORK}"
+    npx hardhat run migrations/deploySkaleManagerComponents.ts --network "${RUNNING_NETWORK}"
 fi
 exit
