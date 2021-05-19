ed905642da9af6dc02afbbf1be3fd08c6344e5df
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-05-19 13:12:55 +0300

    SKALE-3418 Add Logger and ConfigReader

--- proxy/contracts/test/ConfigReader.sol
+++ proxy/contracts/test/ConfigReader.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 /**
- *   SkaleFeatures.sol - SKALE Interchain Messaging Agent
- *   Copyright (C) 2019-Present SKALE Labs
- *   @author Sergiy Lavrynenko
+ *   ConfigReader.sol - SKALE Interchain Messaging Agent
+ *   Copyright (C) 2021-Present SKALE Labs
+ *   @author Dmytro Stebaiev
  *
  *   SKALE IMA is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU Affero General Public License as published
@@ -19,14 +19,20 @@
  *   along with SKALE IMA.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+
 pragma solidity 0.6.12;
 
+import "../schain/tokens/EthERC20.sol";
 
-contract SkaleFeatures {
 
+contract ConfigReader {
     uint256 public constant FREE_MEM_PTR = 0x40;
+    uint256 public constant FN_NUM_LOG_TEXT_MESSAGE = 0x12;
     uint256 public constant FN_NUM_GET_CONFIG_VARIABLE_UINT256 = 0x13;
     uint256 public constant FN_NUM_GET_CONFIG_VARIABLE_ADDRESS = 0x14;
+    uint256 public constant FN_NUM_GET_CONFIG_VARIABLE_STRING = 0x15;
+    uint256 public constant FN_NUM_RESERVED = 0x16;
+    uint256 public constant FN_NUM_GET_CONFIG_PERMISSION_FLAG = 0x17;
 
     function getConfigVariableUint256(
         string memory strConfigVariableName
@@ -77,5 +83,49 @@ contract SkaleFeatures {
         }
         require(success, "Get config address failed");
     }
-}
 
+    function getConfigVariableString( string memory strConfigVariableName ) public view returns ( string memory rv ) {
+        uint256 fmp = FREE_MEM_PTR;
+        uint256 blocks = (bytes(strConfigVariableName).length + 31) / 32 + 1;
+        bool success;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            let ptr := mload(fmp)
+            for { let i := 0 } lt( i, blocks ) { i := add(1, i) } {
+                let where := add(ptr, mul(32, i))
+                let what := mload(add(strConfigVariableName, mul(32, i)))
+                mstore(where, what)
+            }
+            success := staticcall(
+                not(0),
+                FN_NUM_GET_CONFIG_VARIABLE_STRING,
+                ptr,
+                mul( blocks, 32 ),
+                rv,
+                mul( 1024, 1024 )
+            )
+        }
+        require(success, "Get config string failed");
+    }
+
+    function getConfigPermissionFlag(address a, string memory strConfigVariableName) public view returns (uint256 rv) {
+        uint256 fmp = FREE_MEM_PTR;
+        uint256 fnc = FN_NUM_GET_CONFIG_PERMISSION_FLAG;
+        uint256 blocks = (bytes(strConfigVariableName).length + 31) / 32 + 1;
+        bool success;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            let p := mload(fmp)
+            mstore(p, a)
+            let ptr := add(p, 32)
+            for { let i := 0 } lt( i, blocks ) { i := add(1, i) } {
+                let where := add(ptr, mul(32, i))
+                let what := mload(add(strConfigVariableName, mul(32, i)))
+                mstore(where, what)
+            }
+            success := staticcall(not(0), fnc, p, add(64, mul(blocks, 32) ), p, 32)
+            rv := mload(ptr)
+        }
+        require(success, "Get config permission failed");
+    }
+}
