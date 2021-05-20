72f5c9f04a865f1093dfbbf36c297e10a81265eb
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-05-20 16:44:13 +0300

    SKALE-3418 Update paths

--- proxy/predeployed/scripts/build_package.sh
+++ proxy/predeployed/scripts/build_package.sh
@@ -3,8 +3,9 @@
 set -e
 
 cd "$(dirname "$0")/.."
+ARTIFACTS_DIR="src/ima_predeployed/artifacts/"
 mkdir -p src/ima_predeployed/artifacts/
-cp -v ../node_modules/@openzeppelin/upgrades/build/contracts/ProxyAdmin.json src/ima_predeployed/artifacts/
-cp -v ../node_modules/@openzeppelin/upgrades/build/contracts/AdminUpgradeabilityProxy.json src/ima_predeployed/artifacts/
-cp -v ../build/contracts/*.json src/ima_predeployed/artifacts/
+cp -v ../node_modules/@openzeppelin/upgrades-core/artifacts/ProxyAdmin.json $ARTIFACTS_DIR
+cp -v ../node_modules/@openzeppelin/upgrades-core/artifacts/AdminUpgradeabilityProxy.json $ARTIFACTS_DIR
+cp -v ../build/contracts/schain/*/*.json $ARTIFACTS_DIR
 python3 -m build
