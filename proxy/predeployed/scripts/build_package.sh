95bff0c5dd66868fc07aabd8023be344f10d745c
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-05-27 18:54:36 +0300

    SKALE-3418 Add version number generation

--- proxy/predeployed/scripts/build_package.sh
+++ proxy/predeployed/scripts/build_package.sh
@@ -3,6 +3,7 @@
 set -e
 
 cd "$(dirname "$0")/.."
+./scripts/generate_package_version.py > version.txt
 ARTIFACTS_DIR="src/ima_predeployed/artifacts/"
 mkdir -p src/ima_predeployed/artifacts/
 cp -v ../node_modules/@openzeppelin/upgrades-core/artifacts/ProxyAdmin.json $ARTIFACTS_DIR
--- proxy/predeployed/scripts/publish_package.sh
+++ proxy/predeployed/scripts/publish_package.sh
@@ -3,5 +3,4 @@
 set -e
 
 cd "$(dirname "$0")/.."
-./scripts/generate_package_version.py > version.txt
 python3 -m twine upload --repository testpypi -u __token__ -p "$PYPI_TOKEN" dist/*
