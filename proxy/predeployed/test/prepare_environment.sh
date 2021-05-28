f814e1042a4cf592efd503380216f379d28d42f1
Dmytro Stebaiev <dimastebaev@gmail.com>
2021-05-28 17:25:00 +0300

    SKALE-3418 Fix error with package version

--- proxy/predeployed/test/prepare_environment.sh
+++ proxy/predeployed/test/prepare_environment.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")/.."
 
 python3 -m venv test/venv
 source test/venv/bin/activate
-pip install dist/ima-predeployed-*.tar.gz
+pip install dist/ima_predeployed-*.whl
 BLOCKCHAIN_DIR="/tmp/blockchain/"
 python test/generate_genesis.py test/base_genesis.json test/config.json > test/genesis.json
 mkdir "$BLOCKCHAIN_DIR"
