68506c7fc09842ae27039f9a0e17f7a5e457a7e4
Artem Payvin <payvinartem@gmail.com>
2021-07-06 20:49:42 +0300

    SKALE-4292 fix test and coverage

--- proxy/test/DepositBoxEth.ts
+++ proxy/test/DepositBoxEth.ts
@@ -104,7 +104,7 @@ describe("DepositBoxEth", () => {
         await messageProxy.grantRole(await messageProxy.EXTRA_CONTRACT_REGISTRAR_ROLE(), deployer.address);
         await initializeSchain(contractManager, schainName, user.address, 1, 1);
         await rechargeSchainWallet(contractManager, schainName, user2.address, "1000000000000000000");
-        await messageProxy.registerExtraContract(schainName, depositBoxEth.address);
+        await messageProxy.registerExtraContractForAll(depositBoxEth.address);
         await messageProxy.registerExtraContract(schainName, communityPool.address);
         await messageProxy.registerExtraContract(schainName, linker.address);
     });
