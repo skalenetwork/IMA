b47262346db8a9c4793f8cf6e645893d3de718c4
Chadwick Strange <cstrangedk@users.noreply.github.com>
2021-08-03 19:56:36 -0700

    Generate docs

---
+++ proxy/scripts/gen-nav.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+// cSpell:words startcase
+
+const path = require('path');
+const proc = require('child_process');
+const startCase = require('lodash.startcase');
+
+const baseDir = process.argv[2];
+
+console.log('.API');
+
+function getPageTitle (directory) {
+  return startCase(directory);
+}
+
+const files = proc.execFileSync(
+  'find', [baseDir, '-type', 'f'], { encoding: 'utf8' }
+).split('\n').filter(s => s !== '');
+
+const links = files.map((file) => {
+  const doc = file.replace(baseDir, '').replace(/^\/|\/$/g, '');
+  const title = path.parse(file).name;
+  // console.log(`* xref:${doc}[${getPageTitle(title)}]`);
+
+  return {
+    xref: `* xref:${doc}[${getPageTitle(title)}]`,
+    title,
+  };
+});
+
+// Case-insensitive sort based on titles (so 'token/ERC20' gets sorted as 'erc20')
+const sortedLinks = links.sort(function (a, b) {
+  return a.title.toLowerCase().localeCompare(b.title.toLowerCase(), undefined, { numeric: true });
+});
+
+for (const link of sortedLinks) {
+  console.log(link.xref);
+}
---
+++ proxy/scripts/prepare-docs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# cSpell:words adoc
+
+set -o errexit
+
+OUTDIR=../docs/modules/api/pages/
+
+if [ ! -d node_modules ]; then
+  yarn install --frozen-lockfile
+fi
+
+rm -rf "$OUTDIR"
+
+solidity-docgen \
+  -i contracts/ \
+  -H ../docs/helpers.js \
+  --solc-module=./scripts/prepare-docs-solc.js \
+  -t ../docs \
+  -o "$OUTDIR" \
+  --extension=adoc
+
+node scripts/gen-nav.js "$OUTDIR" > "$OUTDIR/../nav.adoc"
