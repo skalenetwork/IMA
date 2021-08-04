b4980a676b4a25d332deac282eac5053555db8c0
Chadwick Strange <cstrangedk@users.noreply.github.com>
2021-08-03 18:03:44 -0700

    WIP

---
+++ scripts/prepare-docs-solc.js
@@ -0,0 +1,14 @@
+const { getCompilersDir } = require('hardhat/internal/util/global-dir');
+const { CompilerDownloader } = require('hardhat/internal/solidity/compiler/downloader');
+const { Compiler } = require('hardhat/internal/solidity/compiler');
+
+const version = "0.6.10";
+
+async function getSolc () {  
+  const downloader = new CompilerDownloader(await getCompilersDir(), { forceSolcJs: true });
+  const { compilerPath } = await downloader.getDownloadedCompilerPath(version);
+  const compiler = new Compiler(compilerPath);
+  return compiler.getSolc();
+}
+
+module.exports = Object.assign(getSolc(), { __esModule: true });
