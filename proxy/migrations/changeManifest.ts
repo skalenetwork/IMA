a42a22f58d973f5b9205c5ee460c7f2f7d9577d5
Artem Payvin <payvinartem@gmail.com>
2021-10-21 14:48:53 +0300

    SKALE-4626 fix codacy and test_upgrade

--- proxy/migrations/changeManifest.ts
+++ proxy/migrations/changeManifest.ts
@@ -4,31 +4,31 @@ import { importAddresses, generateManifest } from "./generateManifest";
 
 export async function change() {
 
-    if (!process.env.ABI) {
-        console.log(chalk.red("Set path to file with ABI and addresses to ABI environment variables"));
+    if( !process.env.ABI ) {
+        console.log( chalk.red( "Set path to file with ABI and addresses to ABI environment variables" ) );
         return;
     }
 
-    if (!process.env.MANIFEST) {
-        console.log(chalk.red("Set path to file with Manifest to MANIFEST environment variables"));
+    if( !process.env.MANIFEST ) {
+        console.log( chalk.red( "Set path to file with Manifest to MANIFEST environment variables" ) );
         return;
     }
 
     const abiFilename = process.env.ABI;
     const manifestFilename = process.env.MANIFEST;
-    const currentAbi = JSON.parse(await fs.readFile(abiFilename, "utf-8"));
-    const currentManifest = JSON.parse(await fs.readFile(manifestFilename, "utf-8"));
+    const currentAbi = JSON.parse( await fs.readFile( abiFilename, "utf-8" ) );
+    const currentManifest = JSON.parse( await fs.readFile( manifestFilename, "utf-8" ) );
 
-    const addresses = await importAddresses(currentManifest, currentAbi);
-    const newManifest = await generateManifest(addresses);
+    const addresses = await importAddresses( currentManifest, currentAbi );
+    const newManifest = await generateManifest( addresses );
     return newManifest;
 }
 
-if (require.main === module) {
+if( require.main === module ) {
     change()
-        .then(() => process.exit(0))
-        .catch(error => {
-            console.error(error);
-            process.exit(1);
-        });
+        .then( () => process.exit( 0 ) )
+        .catch( error => {
+            console.error( error );
+            process.exit( 1 );
+        } );
 }
