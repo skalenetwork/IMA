698e26484abcd5d1a91e2e941be898674b4d31f4
Vadim Yavorsky <yavrsky@gmail.com>
2021-11-29 17:59:44 +0400

    SKALE-4682 Fix indentations

--- proxy/predeployed/test/generate_genesis.py
+++ proxy/predeployed/test/generate_genesis.py
@@ -16,21 +16,21 @@ def main():
         target_key = sys.argv[-1]
     with open(base_genesis_filename) as base_genesis_file:
         with open(config_filename) as config_file:
-                base_genesis = json.load(base_genesis_file)
-                config = json.load(config_file)
-                genesis = base_genesis
-                genesis[target_key].update(generate_contracts(
-                    config['schain_owner'],
-                    config['schain_name'],
-                    {
-                        'deposit_box_eth_address': config['eth_deposit_box'],
-                        'deposit_box_erc20_address': config['erc20_deposit_box'],
-                        'deposit_box_erc721_address': config['erc721_deposit_box'],
-                        'deposit_box_erc1155_address': config['erc1155_deposit_box'],
-                        'linker_address': config['linker'],
-                        'community_pool_address': config['community_pool']
-                    }))
-                print(json.dumps(genesis, indent=4, sort_keys=True))
+            base_genesis = json.load(base_genesis_file)
+            config = json.load(config_file)
+            genesis = base_genesis
+            genesis[target_key].update(generate_contracts(
+                config['schain_owner'],
+                config['schain_name'],
+                {
+                    'deposit_box_eth_address': config['eth_deposit_box'],
+                    'deposit_box_erc20_address': config['erc20_deposit_box'],
+                    'deposit_box_erc721_address': config['erc721_deposit_box'],
+                    'deposit_box_erc1155_address': config['erc1155_deposit_box'],
+                    'linker_address': config['linker'],
+                    'community_pool_address': config['community_pool']
+                }))
+            print(json.dumps(genesis, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
