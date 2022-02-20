3ec844207a2dad90a484576d3d40bec60c1aab94
Sergiy Lavrynenko <sergiy@skalelabs.com>
2022-02-20 11:58:40 +0200

    SKALE-4977 geth startup check

--- proxy/predeployed/test/test.sh
+++ proxy/predeployed/test/test.sh
@@ -6,7 +6,11 @@ cd "$(dirname "$0")"
 
 geth --datadir /tmp/blockchain/ --dev --http &
 GETH_PID=$!
+echo ----- GETH_PID=$GETH_PID
+echo "Sleeping to let geth start..."
 sleep 20
+echo "Checking geth is running..."
+ps -A | grep geth
 
 source venv/bin/activate
 pip install -r requirements.txt
