f4cda2d3ac632a8f9ccf95ab37e3c4c527f86a91
Polina Kiporenko <101632617+PolinaKiporenko@users.noreply.github.com>
2022-05-03 17:01:39 +0100

    Add erc 721 register-origin-imajs.js

---
+++ docs/modules/ROOT/examples/s2s/erc721/register-origin-imajs.js
@@ -0,0 +1,26 @@
+// import & init ima-js here
+
+export async function linkERC721TokenOrigin(ima) {
+    let erc721OnOrigin = "[ADDRESS_OF_ERC721_TOKEN_ON_ORIGIN]";
+    let erc721OnTarget = "[ADDRESS_OF_ERC721_TOKEN_ON_TARGET]";
+
+    const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+    let address = "[YOUR_ADDRESS]";
+    let privateKey = "[YOUR_PRIVATE_KEY]";
+
+    let opts = {
+        address: address,
+        privateKey: privateKey // remove privateKey from txOpts to use Metamask signing
+    };
+
+    const isERC721AddedOrigin = await ima.schain.erc721.isTokenAdded(erc721OnOrigin);
+    if (isERC721AddedOrigin === ZERO_ADDRESS) {
+        await ima.schain.erc721.addTokenByOwner(
+            targetChainName,
+            erc721OnOrigin,
+            erc721OnTarget,
+            opts
+        );
+    }
+}
