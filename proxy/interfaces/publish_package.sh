18d75364c7cc07924b11d7e3b90cf90d5471eb54
Artem Payvin <payvinartem@gmail.com>
2022-06-12 22:57:09 +0100

    Fix codacy'

--- proxy/interfaces/publish_package.sh
+++ proxy/interfaces/publish_package.sh
@@ -11,13 +11,13 @@ then
 fi
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-ROOT_DIR=$(dirname $SCRIPT_DIR)
+ROOT_DIR="$(dirname $SCRIPT_DIR)"
 INTERFACES_DIR="$ROOT_DIR/contracts/interfaces"
 
 cd "$ROOT_DIR"
 
-BRANCH=$(echo $BRANCH | tr [:upper:] [:lower:] | tr -d [:space:])
-VERSION=$(BRANCH=$BRANCH $ROOT_DIR/predeployed/scripts/calculate_version.sh)
+BRANCH="$(echo $BRANCH | tr [:upper:] [:lower:] | tr -d [:space:])"
+VERSION="$(BRANCH=$BRANCH $ROOT_DIR/predeployed/scripts/calculate_version.sh)"
 
 TAG=""
 if ! [[ $BRANCH == 'stable' ]]
@@ -36,4 +36,4 @@ cp LICENSE "$INTERFACES_DIR"
 cp README.md "$INTERFACES_DIR"
 cp "$ROOT_DIR/interfaces/package.json" "$INTERFACES_DIR"
 
-yarn publish $INTERFACES_DIR --access public --new-version $VERSION --verbose --no-git-tag-version $TAG --ignore-scripts
+yarn publish "$INTERFACES_DIR" --access public --new-version "$VERSION" --verbose --no-git-tag-version "$TAG" --ignore-scripts
