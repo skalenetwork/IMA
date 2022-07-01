78ec914f9f82645f4b79591783f540bd96f56ba0
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2022-07-01 18:01:39 +0300

    Update CommunityLocker

--- proxy/predeployed/scripts/build_package.sh
+++ proxy/predeployed/scripts/build_package.sh
@@ -4,10 +4,10 @@ set -e
 
 cd "$(dirname "$0")/.."
 ./scripts/generate_package_version.py > version.txt
-ARTIFACTS_DIR="src/ima_predeployed/artifacts/"
-mkdir -p src/ima_predeployed/artifacts/
-cp -v ../node_modules/@openzeppelin/upgrades-core/artifacts/ProxyAdmin.json $ARTIFACTS_DIR
-cp -v ../node_modules/@openzeppelin/upgrades-core/artifacts/AdminUpgradeabilityProxy.json $ARTIFACTS_DIR
-cp -v ../artifacts/contracts/schain/*/*.json $ARTIFACTS_DIR
-cp -v ../artifacts/contracts/schain/*/*/*.json $ARTIFACTS_DIR
-python3 -m build
+#ARTIFACTS_DIR="src/ima_predeployed/artifacts/"
+#mkdir -p src/ima_predeployed/artifacts/
+#cp -v ../node_modules/@openzeppelin/upgrades-core/artifacts/ProxyAdmin.json $ARTIFACTS_DIR
+#cp -v ../node_modules/@openzeppelin/upgrades-core/artifacts/AdminUpgradeabilityProxy.json $ARTIFACTS_DIR
+#cp -v ../artifacts/contracts/schain/*/*.json $ARTIFACTS_DIR
+#cp -v ../artifacts/contracts/schain/*/*/*.json $ARTIFACTS_DIR
+#python3 -m build
--- proxy/predeployed/src/ima_predeployed/contracts/community_locker.py
+++ proxy/predeployed/src/ima_predeployed/contracts/community_locker.py
@@ -1,10 +1,15 @@
+from os.path import join, dirname
+from typing import Dict
+
 from ima_predeployed.addresses import MESSAGE_PROXY_FOR_SCHAIN_ADDRESS, TOKEN_MANAGER_LINKER_ADDRESS
-from ima_predeployed.contract_generator import ContractGenerator, next_slot
-from web3 import Web3
+from predeployed_generator.openzeppelin.access_control_enumerable_generator import (
+    AccessControlEnumerableGenerator
+)
 
 
-class CommunityLockerGenerator(ContractGenerator):
-    ARTIFACT_FILENAME = "CommunityLocker.json"
+class CommunityLockerGenerator(AccessControlEnumerableGenerator):
+    ARTIFACT_FILENAME = 'CommunityLocker.json'
+    META_FILENAME = 'CommunityLocker.json'
     DEFAULT_ADMIN_ROLE = (0).to_bytes(32, 'big')
     DEFAULT_TIME_LIMIT_SEC = 5 * 60
 
@@ -42,22 +47,32 @@ class CommunityLockerGenerator(ContractGenerator):
     ROLES_SLOT = 101
     ROLE_MEMBERS_SLOT = 151
     MESSAGE_PROXY_SLOT = 201
-    TOKEN_MANAGER_LINKER_SLOT = next_slot(MESSAGE_PROXY_SLOT)
-    COMMUNITY_POOL_SLOT = next_slot(TOKEN_MANAGER_LINKER_SLOT)
-    SCHAIN_HASH_SLOT = next_slot(COMMUNITY_POOL_SLOT)
-    TIME_LIMIT_PER_MESSAGE_SLOT = next_slot(SCHAIN_HASH_SLOT)
+    TOKEN_MANAGER_LINKER_SLOT = AccessControlEnumerableGenerator.next_slot(MESSAGE_PROXY_SLOT)
+    COMMUNITY_POOL_SLOT = AccessControlEnumerableGenerator.next_slot(TOKEN_MANAGER_LINKER_SLOT)
+    SCHAIN_HASH_SLOT = AccessControlEnumerableGenerator.next_slot(COMMUNITY_POOL_SLOT)
+    TIME_LIMIT_PER_MESSAGE_SLOT = AccessControlEnumerableGenerator.next_slot(SCHAIN_HASH_SLOT)
 
-    def __init__(self, deployer_address: str, schain_name: str, community_pool_address: str):
-        super().__init__(self.ARTIFACT_FILENAME)
-        self._setup(deployer_address, schain_name, community_pool_address)
+    def __init__(self):
+        generator = CommunityLockerGenerator.from_hardhat_artifact(
+            join(dirname(__file__), 'artifacts', self.ARTIFACT_FILENAME),
+            join(dirname(__file__), 'artifacts', self.META_FILENAME))
+        super().__init__(bytecode=generator.bytecode, abi=generator.abi, meta=generator.meta)
 
-    # private
+    @classmethod
+    def generate_storage(cls, **kwargs) -> Dict[str, str]:
+        deployer_address = kwargs['deployer_address']
+        schain_name = kwargs['schain_name']
+        community_pool_address = kwargs['community_pool_address']
 
-    def _setup(self, deployer_address: str, schain_name: str, community_pool_address) -> None:
-        self._write_uint256(self.INITIALIZED_SLOT, 1)
-        self._setup_role(self.ROLES_SLOT, self.ROLE_MEMBERS_SLOT, self.DEFAULT_ADMIN_ROLE, [deployer_address])
-        self._write_address(self.MESSAGE_PROXY_SLOT, MESSAGE_PROXY_FOR_SCHAIN_ADDRESS)
-        self._write_address(self.TOKEN_MANAGER_LINKER_SLOT, TOKEN_MANAGER_LINKER_ADDRESS)
-        self._write_address(self.COMMUNITY_POOL_SLOT, community_pool_address)
-        self._write_bytes32(self.SCHAIN_HASH_SLOT, Web3.solidityKeccak(['string'], [schain_name]))
-        self._write_uint256(self.TIME_LIMIT_PER_MESSAGE_SLOT, self.DEFAULT_TIME_LIMIT_SEC)
+        storage: Dict[str, str] = {}
+        roles_slots = cls.RolesSlots(roles=cls.ROLES_SLOT, role_members=cls.ROLE_MEMBERS_SLOT)
+
+        cls._write_uint256(storage, cls.INITIALIZED_SLOT, 1)
+        cls._setup_role(storage, roles_slots, cls.DEFAULT_ADMIN_ROLE, [deployer_address])
+        cls._write_address(storage, cls.MESSAGE_PROXY_SLOT, MESSAGE_PROXY_FOR_SCHAIN_ADDRESS)
+        cls._write_address(storage, cls.TOKEN_MANAGER_LINKER_SLOT, TOKEN_MANAGER_LINKER_ADDRESS)
+        cls._write_address(storage, cls.COMMUNITY_POOL_SLOT, community_pool_address)
+        cls._write_string(storage, cls.SCHAIN_HASH_SLOT, schain_name)
+        cls._write_uint256(storage, cls.TIME_LIMIT_PER_MESSAGE_SLOT, cls.DEFAULT_TIME_LIMIT_SEC)
+
+        return storage
