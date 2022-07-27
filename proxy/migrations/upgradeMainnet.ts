6ee2e98daf518ed54acba4f9e7517bc59a97a240
Artem Payvin <payvinartem@gmail.com>
2022-07-27 12:41:12 +0100

    Add upgrade function

--- proxy/migrations/upgradeMainnet.ts
+++ proxy/migrations/upgradeMainnet.ts
@@ -1,11 +1,48 @@
-import { contracts } from "./deployMainnet";
+import { getManifestAdmin } from "@openzeppelin/hardhat-upgrades/dist/admin";
+import { ethers } from "hardhat";
+import { MessageProxyForMainnet } from "../typechain/MessageProxyForMainnet";
+import { contracts, getContractKeyInAbiFile } from "./deployMainnet";
+import { encodeTransaction } from "./tools/multiSend";
 import { upgrade } from "./upgrade";
+import chalk from "chalk";
 
 async function main() {
     await upgrade(
         "1.3.0",
         contracts,
-        async (safeTransactions, abi) => undefined,
+        async (safeTransactions, abi) => {
+            const proxyAdmin = await getManifestAdmin(hre);
+             const owner = await proxyAdmin.owner();
+             const messageProxyForMainnetName = "MessageProxyForMainnet";
+             const messageProxyForMainnetFactory = await ethers.getContractFactory(messageProxyForMainnetName);
+             const messageProxyForMainnetAddress = abi[getContractKeyInAbiFile(messageProxyForMainnetName) + "_address"];
+             let messageProxyForMainnet;
+             if (messageProxyForMainnetAddress) {
+                 messageProxyForMainnet = messageProxyForMainnetFactory.attach(messageProxyForMainnetAddress) as MessageProxyForMainnet;
+                 const constantSetterRole = await messageProxyForMainnet.CONSTANT_SETTER_ROLE();
+                 const isHasRole = await messageProxyForMainnet.hasRole(constantSetterRole, owner);
+                 if (!isHasRole) {
+                     console.log(chalk.yellow("Prepare transaction to grantRole CONSTANT_SETTER_ROLE to " + owner));
+                     safeTransactions.push(encodeTransaction(
+                         0,
+                         messageProxyForMainnetAddress,
+                         0,
+                         messageProxyForMainnet.interface.encodeFunctionData("grantRole", [constantSetterRole, owner])
+                     ));
+                 }
+                 console.log(chalk.yellow("Prepare transaction to set header message gas cost to 78888"));
+                 safeTransactions.push(encodeTransaction(
+                     0,
+                     messageProxyForMainnetAddress,
+                     0,
+                     messageProxyForMainnet.interface.encodeFunctionData("setNewHeaderMessageGasCost", [78888])
+                 ));
+             } else {
+                 console.log(chalk.red("MessageProxyForMainnet was not found!"));
+                 console.log(chalk.red("Check your abi!!!"));
+                 process.exit(1);
+             }
+        },
         async (safeTransactions, abi) => undefined,
         "proxyMainnet"
     );
@@ -19,3 +56,7 @@ if( require.main === module ) {
             process.exit( 1 );
         } );
 }
+function hre(hre: any) {
+    throw new Error("Function not implemented.");
+}
+
