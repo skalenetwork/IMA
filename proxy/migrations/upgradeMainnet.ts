04fefc9e5e36d180efb05c8b1fe21137cd5940d0
Dmytro Stebaiev <dmytro@skalelabs.com>
2022-08-11 17:14:06 +0300

    Fix gas reimbursement

--- proxy/migrations/upgradeMainnet.ts
+++ proxy/migrations/upgradeMainnet.ts
@@ -1,12 +1,27 @@
-import { contracts } from "./deployMainnet";
+import { MessageProxyForMainnet } from "../typechain";
+import { contracts, getContractKeyInAbiFile } from "./deployMainnet";
+import { encodeTransaction } from "./tools/multiSend";
 import { upgrade } from "./upgrade";
+import { ethers } from "hardhat";
 
 async function main() {
     await upgrade(
         "1.3.0",
         contracts,
         async (safeTransactions, abi) => undefined,
-        async (safeTransactions, abi) => undefined,
+        async (safeTransactions, abi) => {
+            const messageProxyForMainnet = (await ethers.getContractFactory("MessageProxyForMainnet"))
+            .attach(abi[getContractKeyInAbiFile("MessageProxyForMainnet") + "_address"]) as MessageProxyForMainnet;
+
+            safeTransactions.push(encodeTransaction(
+                0,
+                messageProxyForMainnet.address,
+                0,
+                messageProxyForMainnet.interface.encodeFunctionData(
+                    "setNewHeaderMessageGasCost",
+                    [ await messageProxyForMainnet.headerMessageGasCost() ]),
+            ));
+        },
         "proxyMainnet"
     );
 }
