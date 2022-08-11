371231641cadb04709272d2469dcc67e3753f5b3
Dmytro Stebaiev <dmytro@skalelabs.com>
2022-08-11 17:14:06 +0300

    Fix gas reimbursement

--- proxy/migrations/upgradeMainnet.ts
+++ proxy/migrations/upgradeMainnet.ts
@@ -2,10 +2,7 @@ import { MessageProxyForMainnet } from "../typechain";
 import { contracts, getContractKeyInAbiFile } from "./deployMainnet";
 import { encodeTransaction } from "./tools/multiSend";
 import { upgrade } from "./upgrade";
-import { getManifestAdmin } from "@openzeppelin/hardhat-upgrades/dist/admin";
 import { ethers } from "hardhat";
-import hre from "hardhat";
-import chalk from "chalk";
 
 async function main() {
     await upgrade(
@@ -13,38 +10,16 @@ async function main() {
         contracts,
         async (safeTransactions, abi) => undefined,
         async (safeTransactions, abi) => {
-            const proxyAdmin = await getManifestAdmin(hre);
-            const owner = await proxyAdmin.owner();
             const messageProxyForMainnet = (await ethers.getContractFactory("MessageProxyForMainnet"))
             .attach(abi[getContractKeyInAbiFile("MessageProxyForMainnet") + "_address"]) as MessageProxyForMainnet;
 
-            if (! await messageProxyForMainnet.hasRole(await messageProxyForMainnet.CONSTANT_SETTER_ROLE(), owner)) {
-                console.log(chalk.yellow("Prepare transaction to grantRole CONSTANT_SETTER_ROLE to " + owner));
-                safeTransactions.push(encodeTransaction(
-                    0,
-                    messageProxyForMainnet.address,
-                    0,
-                    messageProxyForMainnet.interface.encodeFunctionData(
-                        "grantRole",
-                        [ await messageProxyForMainnet.CONSTANT_SETTER_ROLE(), owner ]
-                    )
-                ));
-            }
-
-            const newHeaderMessageGasCost = 92251;
-
-            console.log(chalk.yellow(
-                "Prepare transaction to set header message gas cost to",
-                newHeaderMessageGasCost.toString()
-            ));
             safeTransactions.push(encodeTransaction(
                 0,
                 messageProxyForMainnet.address,
                 0,
                 messageProxyForMainnet.interface.encodeFunctionData(
                     "setNewHeaderMessageGasCost",
-                    [ newHeaderMessageGasCost ]
-                )
+                    [ await messageProxyForMainnet.headerMessageGasCost() ]),
             ));
         },
         "proxyMainnet"
