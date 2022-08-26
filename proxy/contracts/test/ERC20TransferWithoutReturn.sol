d32cee9c4901f87528470d58e62f4ccbb30deef8
Artem Payvin <payvinartem@gmail.com>
2022-08-26 22:34:16 +0100

    Add tests

---
+++ proxy/contracts/test/ERC20TransferWithoutReturn.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+pragma solidity 0.8.16;
+
+import "./ERC20WithoutTransfer.sol";
+
+interface IERC20TransferWithoutReturn is IERC20WithoutTransfer {
+    function transferFrom(address sender, address recipient, uint256 amount) external;
+}
+
+contract ERC20TransferWithoutReturn is IERC20TransferWithoutReturn, ERC20WithoutTransfer {
+
+    // solhint-disable-next-line no-empty-blocks
+    constructor(string memory tokenName, string memory tokenSymbol) ERC20WithoutTransfer(tokenName, tokenSymbol) {}
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override {
+        _transfer(sender, recipient, amount);
+        _approve(
+            sender,
+            msg.sender,
+            allowance(sender, msg.sender) - amount
+        );
+    }
+}
