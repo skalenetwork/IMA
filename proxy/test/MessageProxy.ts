8d164f4d860f5bda982f7071d7370b0be4b444d6
Artem Payvin <payvinartem@gmail.com>
2022-09-23 12:34:11 +0100

    Remove only

--- proxy/test/MessageProxy.ts
+++ proxy/test/MessageProxy.ts
@@ -446,7 +446,7 @@ describe("MessageProxy", () => {
             incomingMessagesCounter.should.be.deep.equal(BigNumber.from(4));
         });
 
-        it.only("should post incoming message and reimburse from CommunityPool", async () => {
+        it("should post incoming message and reimburse from CommunityPool", async () => {
             const startingCounter = 0;
             await initializeSchain(contractManager, schainName, deployer.address, 1, 1);
             const nodeCreationParams = {
