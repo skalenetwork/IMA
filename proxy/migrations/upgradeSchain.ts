124bb33343f073384db7e465076bf2d5c61737be
Dmytro Stebaiev <dmytro@skalelabs.com>
2023-02-16 20:28:40 +0200

    Update upgrade script

--- proxy/migrations/upgradeSchain.ts
+++ proxy/migrations/upgradeSchain.ts
@@ -19,30 +19,13 @@ async function main() {
     const pathToManifest: string = stringValue(process.env.MANIFEST);
     await manifestSetup( pathToManifest );
     await upgrade(
-        "1.3.4",
+        "1.4.0",
         contracts,
         async (safeTransactions, abi) => {
             // deploying of new contracts
         },
         async( safeTransactions, abi ) => {
-            const communityLockerName = "CommunityLocker";
-            const communityLockerFactory = await ethers.getContractFactory(communityLockerName);
-            const communityLockerAddress = abi[getContractKeyInAbiFile(communityLockerName) + "_address"];
-            let communityLocker;
-            if (communityLockerAddress) {
-                communityLocker = communityLockerFactory.attach(communityLockerAddress) as CommunityLocker;
-                console.log(chalk.yellow("Prepare transaction to initialize timestamp"));
-                safeTransactions.push(encodeTransaction(
-                    0,
-                    communityLockerAddress,
-                    0,
-                    communityLocker.interface.encodeFunctionData("initializeTimestamp")
-                ));
-            } else {
-                console.log(chalk.red("CommunityLocker was not found!"));
-                console.log(chalk.red("Check your abi!!!"));
-                process.exit(1);
-            }
+            // do initialization
         },
         "proxySchain"
     );
