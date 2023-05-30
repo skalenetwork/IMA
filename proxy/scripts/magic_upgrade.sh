86159cb7a5dd4f4fe7f310a231a9da64343b2006
Vadim Yavorsky <yavrsky@gmail.com>
2023-05-30 20:42:02 +0300

    Add additional steps

--- proxy/scripts/magic_upgrade.sh
+++ proxy/scripts/magic_upgrade.sh
@@ -4,22 +4,24 @@ set -e
 
 
 if [[ -z $DEPLOYED_VERSION ]]; then
-  echo "Set DEPLOYED_VERSION"
-  exit 1
+    echo "Set DEPLOYED_VERSION"
+    exit 1
+elif [[ ! $DEPLOYED_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+-(beta|stable)\.[0-9]+$ ]]; then
+    echo "Version format is not valid"
+    exit 1
 elif [[ -z $SCHAIN_ID ]]; then
-  echo "Set SCHAIN_ID"
-  exit 1
+    echo "Set SCHAIN_ID"
+    exit 1
 elif [[ -z $SCHAIN_NAME ]]; then
-  echo "Set SCHAIN_NAME"
-  exit 1
+    echo "Set SCHAIN_NAME"
+    exit 1
 elif [[ -z $SAFE_ADDRESS ]]; then
-  echo "Set SAFE_ADDRESS"
-  exit 1
+    echo "Set SAFE_ADDRESS"
+    exit 1
 elif [[ -z $ALLOW_NOT_ATOMIC_UPGRADE ]]; then
-  echo "Set ALLOW_NOT_ATOMIC_UPGRADE"
-  exit 1
+    echo "Set ALLOW_NOT_ATOMIC_UPGRADE"
+    exit 1
 fi
-
 
 IMA_RELEASES_URL="https://github.com/skalenetwork/IMA/releases/download/"
 cd data/
@@ -45,8 +47,20 @@ if [[ $MAINNET_CHAIN_ID != "1" ]]; then
     fi
 fi
 
-
-#sed version inside upgrade script
+deployed=$(cat DEPLOYED)
+if [[ $deployed =~ ^([0-9]+\.[0-9]+\.[0-9]+)-stable\.[0-9]+$ ]]; then
+    extracted_version="${BASH_REMATCH[1]}"
+    original_contents=$(cat migrations/upgradeSchain.ts)
+    sed -i "s/\"$extracted_version\"/\"$DEPLOYED_VERSION\"/g" migrations/upgradeSchain.ts
+    updated_contents=$(cat migrations/upgradeSchain.ts)
+    if [ "$original_contents" = "$updated_contents" ]; then
+        echo "Version replacement did not occur."
+        exit 1
+    fi
+else
+    echo "Version format is not valid"
+    exit 1
+fi
 
 SCHAIN_NAME=$SCHAIN_NAME \
 ABI="data/ima-$DEPLOYED_VERSION-predeployed-abi.json" \
@@ -54,4 +68,5 @@ IMA_ABI="data/ima-$MAINNET_STABLE_IMA_VERSION-mainnet-abi.json" \
 MAINNET_CHAIN_ID=$MAINNET_CHAIN_ID \
 SAFE_ADDRESS=$SAFE_ADDRESS \
 ALLOW_NOT_ATOMIC_UPGRADE=$ALLOW_NOT_ATOMIC_UPGRADE \
+VERSION=$VERSION \
 npx hardhat run migrations/upgradeSchain.ts --network custom
