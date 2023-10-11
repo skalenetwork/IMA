3b528790885d4ccc5ccb40241aab6d93904ac1be
Sergiy Lavrynenko <sergiy@skalelabs.com>
2023-10-11 16:49:12 +0100

    ticket-1577 IMA Agent moved into it's own repository

--- postinstall.sh
+++ postinstall.sh
@@ -5,12 +5,6 @@ DIRECTORY_WITH_TESTS=$DIRECTORY_OF_THIS_SCRIPT/test
 DIRECTORY_WITH_TEST_TOKENS=$DIRECTORY_OF_THIS_SCRIPT/test-tokens
 
 echo " "
-echo "----- installing in agent ------------------------------------------------------------------"
-echo " "
-cd ./agent
-yarn install
-cd ..
-
 if [ -d "$DIRECTORY_WITH_TESTS" ] 
 then
     echo " "
