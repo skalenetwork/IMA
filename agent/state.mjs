631b5c8d1cd0755dc4428b6a60ca7305a3848227
Sergiy Lavrynenko <sergiy@skalelabs.com>
2023-11-17 18:08:32 +0000

    1h SNB interval

--- agent/state.mjs
+++ agent/state.mjs
@@ -277,7 +277,7 @@ export function get() {
             "isEnabled": true,
             "bParallelModeRefreshSNB": true,
             // seconds to re-discover SKALE network, 0 to disable
-            "secondsToReDiscoverSkaleNetwork": 90 * 60, // 90 minutes
+            "secondsToReDiscoverSkaleNetwork": 1 * 60 * 60,
             "secondsToWaitForSkaleNetworkDiscovered": 5 * 60
         },
 
