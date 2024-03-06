049879ee907335d3acb8a0177d3d140ce998e3d6
Dmytro Stebaiev <dimastebaev@gmail.com>
2024-03-06 17:15:17 +0200

    Add schain ABI

--- proxy/scripts/generateAbi.ts
+++ proxy/scripts/generateAbi.ts
@@ -1,19 +1,35 @@
 import { promises as fs } from 'fs';
-import { contracts } from "../migrations/deployMainnet";
+import { contracts as mainnetContracts } from "../migrations/deployMainnet";
+import { contracts as schainContracts } from "../migrations/deploySchain";
 import { ethers } from "hardhat";
 import { getAbi, getVersion } from '@skalenetwork/upgrade-tools';
 
-async function main() {
+async function writeAbiToFile(contracts: string[], filename: string) {
     const abi: {[name: string]: []} = {};
     for (const contractName of contracts) {
         console.log(`Load ABI of ${contractName}`);
         const factory = await ethers.getContractFactory(contractName);
         abi[contractName] = getAbi(factory.interface);
     }
-    const version = await getVersion();
-    const filename = `data/mainnet-ima-${version}-abi.json`;
     console.log(`Save to ${filename}`)
     await fs.writeFile(filename, JSON.stringify(abi, null, 4));
+}
+
+async function main() {
+    const version = await getVersion();
+    const mainnetFilename = `data/mainnet-ima-${version}-abi.json`;
+    const schainFilename = `data/schain-ima-${version}-abi.json`;
+    await writeAbiToFile(
+        mainnetContracts,
+        mainnetFilename
+    );
+    await writeAbiToFile(
+        [
+            ...schainContracts,
+            "TokenManagerERC721WithMetadata"
+        ],
+        schainFilename
+    );
 }
 
 if (require.main === module) {
