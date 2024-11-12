0bd9c2e1c40f3fc2ed5ed7522172b4e6582fd824
Vadim Yavorsky <yavrsky@gmail.com>
2024-11-12 16:47:00 +0000

    Replace hardhat plugins with updated versions for verification and matchers

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
+import "@nomicfoundation/hardhat-verify";
+import "@nomicfoundation/hardhat-chai-matchers";
 import "@openzeppelin/hardhat-upgrades";
 import "@typechain/hardhat";
 import "solidity-coverage";
